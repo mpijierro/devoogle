7562b3da03558078239ffef7ca1005840d545c8a
mpijierro <mpijierro@gmail.com>
2018-01-04 01:44:06 +0100

    add forelse resource list

--- resources/views/resource/list.blade.php
+++ resources/views/resource/list.blade.php
@@ -1,10 +1,12 @@
-@foreach ($resources as $resource)
+@forelse ($resources as $resource)
 
     <div class="col-xs-12" style="border:1px solid lightgray; margin-bottom: 15px">
         @include('resource.resource_register',   ['resource' => $resource])
     </div>
 
-@endforeach
+@empty
+    No tenemos recursos aquí por ahora.
+@endforelse
 
 
 
