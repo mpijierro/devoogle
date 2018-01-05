2ac7d0b8587a2ff4322ffc801baa3f8fc43ffc6e
mpijierro <mpijierro@gmail.com>
2018-01-05 03:47:25 +0100

    remove unespecified lang

--- database/seeds/LangSeeder.php
+++ database/seeds/LangSeeder.php
@@ -13,13 +13,6 @@ class LangSeeder extends Seeder
     {
 
         DB::table('lang')->insert([
-            'name' => 'Sin especificar',
-            'code' => 'xx',
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
-
-        DB::table('lang')->insert([
             'name' => 'Español',
             'code' => 'es',
             'created_at' => \Carbon\Carbon::now(),
