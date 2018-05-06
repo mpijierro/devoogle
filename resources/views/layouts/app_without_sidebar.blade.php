ccdca8ac827c363d13c926a04566ac2af16f77de
mpijierro <mpijierro@gmail.com>
2018-05-07 00:30:28 +0200

    add blade common for layouts

--- resources/views/layouts/app_without_sidebar.blade.php
+++ resources/views/layouts/app_without_sidebar.blade.php
@@ -9,34 +9,7 @@ use Krucas\Notification\Facades\Notification;
 
     @include('layouts.metas')
 
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
-    @if (isset($loadTagManager))
-
-        <link rel="stylesheet" href="/css/textext.core.css" type="text/css"/>
-        <link rel="stylesheet" href="/css/textext.plugin.tags.css" type="text/css"/>
-        <link rel="stylesheet" href="/css/textext.plugin.autocomplete.css" type="text/css"/>
-        <link rel="stylesheet" href="/css/textext.plugin.prompt.css" type="text/css"/>
-        <script src="/js/textext.core.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/textext.plugin.tags.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/textext.plugin.autocomplete.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/textext.plugin.suggestions.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/textext.plugin.prompt.js" type="text/javascript" charset="utf-8"></script>
-        <script src="/js/textext.plugin.ajax.js" type="text/javascript" charset="utf-8"></script>
-    @endif
-
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap-social.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-
-
+    @include('layouts.header')
 </head>
 <body>
 <div id="app">
