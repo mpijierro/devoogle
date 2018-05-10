83e401a4de98824e77bc9f2b95d0a0426ed13c43
mpijierro <mpijierro@gmail.com>
2018-05-10 02:10:49 +0200

    rename var

--- app/Src/Devoogle/Library/FinderLink.php
+++ app/Src/Devoogle/Library/FinderLink.php
@@ -8,12 +8,11 @@ class FinderLink
     public function find(string $text)
     {
 
-        $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+        $pattern = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 
-        // Check if there is a url in the text
-        if (preg_match($reg_exUrl, $text, $url)) {
+        if (preg_match($pattern, $text, $url)) {
 
-            return preg_replace($reg_exUrl, "<a href='".$url[0]."' target='_blank'>".$url[0]."</a> ", $text);
+            return preg_replace($pattern, "<a href='".$url[0]."' target='_blank'>".$url[0]."</a> ", $text);
         }
 
         return $text;
