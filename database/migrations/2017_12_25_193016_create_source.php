8c111ec4a8553423f3f60e9820660ab00a1e40ad
mpijierro <mpijierro@gmail.com>
2018-05-13 10:27:15 +0200

    add migrations type_source and source

---
+++ database/migrations/2017_12_25_193016_create_source.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSource extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('source', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('type_source_id')->unsigned();
+            $table->foreign('type_source_id')->references('id')->on('type_source');
+
+            $table->string('name');
+            $table->string('slug');
+            $table->datetime('last_time_processed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('source');
+    }
+}
