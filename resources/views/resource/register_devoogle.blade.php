e99dd740ff6208fd6ad7cc00489eddffa29fa23d
mpijierro <mpijierro@gmail.com>
2018-05-29 00:37:55 +0200

    add link to post in medium

--- resources/views/resource/register_devoogle.blade.php
+++ resources/views/resource/register_devoogle.blade.php
@@ -1,6 +1,7 @@
 <div class="row">
     <div class="col-xs-9 col-sm-8 col-md-12">
-        <a href="#" target="_blank" class="resource-title">
+        <a href="https://medium.com/@mpijierro/devoogle-un-agregador-de-recursos-de-programaci%C3%B3n-a73ad9635809"
+           target="_blank" class="resource-title">
             ¿Qué es Devoogle?
         </a>
     </div>
@@ -10,7 +11,7 @@
 <div class="row list-description">
     <div class="col-xs-12 more">
 
-        Si quieres, he escrito un post sobre este proyecto en el que explico con más detalle de que va todo esto.
+        He escrito un post sobre este proyecto en el que explico con más detalle de que va todo esto.
 
     </div>
 </div>
