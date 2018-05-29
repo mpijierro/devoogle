be85b48c2cf12a8d9d6542c5277052187e8f433f
mpijierro <mpijierro@gmail.com>
2018-05-29 23:32:25 +0200

    fix redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * @var \Devoogle\Src\User\Repository\CharacterRepositoryRead
