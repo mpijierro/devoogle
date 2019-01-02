364bf292270fa735ec36f780d24750afdad1a63f
mpijierro <mpijierro@gmail.com>
2019-01-02 22:29:08 +0100

    add YoutubeChannelFactory

---
+++ database/factories/YoutubeChannelFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(\Devoogle\Src\SourceReader\Model\YoutubeChannel::class, function (Faker $faker) {
+
+    $name = $faker->sentence(4);
+
+
+    return [
+        'source_id' => 1,
+        'slug_id' => str_random(),
+        'slug_name' => str_slug($name),
+        'name' => $name,
+        'is_user_channel' => 0,
+        'last_time_processed' => \Carbon\Carbon::now(),
+        'created_at' => \Carbon\Carbon::now(),
+        'updated_at' => \Carbon\Carbon::now(),
+    ];
+});
