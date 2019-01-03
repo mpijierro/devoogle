14b2b4521abc7250b351a3c9e2e6d8bdb440747c
mpijierro <mpijierro@gmail.com>
2019-01-04 00:34:22 +0100

    change email text

--- resources/lang/es/resource.php
+++ resources/lang/es/resource.php
@@ -18,9 +18,8 @@ return [
             'processing' => "Se ha iniciado un proceso para obtener el audio del vídeo :title. Cuando acabe, recibirás un email con un enlace para descargar. Este 
             proceso puede durar unos minutos. Gracias por tu paciencia ;)",
             'subject' => 'Descargar audio: :title',
-            'user_must_be_logged_in' => 'Has sido la primera persona en intentar descargar este recurso por lo que aún no está disponible todavía. 
-            Necesitamos un email al que enviar el enlace de descarga cuando la tengamos disponible. Si quieres, regístrate y vuelve a intentarlo para iniciar 
-            el proceso (tardará unos minutos). Disculpa las molestias, bro.',
+            'user_must_be_logged_in' => 'Has sido la primera persona en intentar descargar este recurso por lo que aún no está disponible. Como el proceso de convertir 
+            el vídeo en audio puede tardar unos minutos, necesitamos un email al que enviar el enlace de descarga una vez haya terminado dicho proceso. Por si acaso, revisa la carpeta spam de tu buzón de correo.  Disculpa las molestias, bro.',
             'resource_not_is_youtube_video' => 'El recurso no es un vídeo de Youtube por lo que, de momento, no es posible convertirlo a audio. Disculpa las molestias, bro',
             'subject_download_exception' => 'Exception al convertir el audio de: :title'
         ]
