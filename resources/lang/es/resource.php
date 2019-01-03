9ffbb999511e0a399e3114e274c8a7075c7507fc
mpijierro <mpijierro@gmail.com>
2019-01-03 23:26:20 +0100

    change text

--- resources/lang/es/resource.php
+++ resources/lang/es/resource.php
@@ -18,7 +18,7 @@ return [
             'processing' => "Se ha iniciado un proceso para obtener el audio del vídeo :title. Cuando acabe, recibirás un email con un enlace para descargar. Este 
             proceso puede durar unos minutos. Gracias por tu paciencia ;)",
             'subject' => 'Descargar audio: :title',
-            'user_must_be_logged_in' => 'Has sido la primera persona en intentar descargar este recurso por lo que no está disponible todavía. 
+            'user_must_be_logged_in' => 'Has sido la primera persona en intentar descargar este recurso por lo que aún no está disponible todavía. 
             Necesitamos un email al que enviar el enlace de descarga cuando la tengamos disponible. Si quieres, regístrate y vuelve a intentarlo para iniciar 
             el proceso (tardará unos minutos). Disculpa las molestias, bro.',
             'resource_not_is_youtube_video' => 'El recurso no es un vídeo de Youtube por lo que, de momento, no es posible convertirlo a audio. Disculpa las molestias, bro',
