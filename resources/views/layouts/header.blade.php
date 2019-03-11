976e84ba44eb48502ea7c58f793a7e45011315bc
mpijierro <mpijierro@gmail.com>
2019-03-12 00:43:40 +0100

    add version to css file

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -28,5 +28,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+<link href="{{ asset('css/custom.css?version=1.0') }}" rel="stylesheet">
 
