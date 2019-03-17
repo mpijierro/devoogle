c4da3da1fe8f76820c336b63a20363c126828204
mpijierro <mpijierro@gmail.com>
2019-03-17 23:58:49 +0100

    remove home text

--- resources/views/resource/home.blade.php
+++ resources/views/resource/home.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row" >
-        <div class="col-xs-12">
-            <h3 class="text-left description-category">
-                <b>Devoogle</b> cuenta actualmente con <b>{{$total}}</b> recursos disponibles.
-            </h3>
-        </div>
-    </div>
-
     @include('resource.list')
 
 @endsection
