760d1cc301d9324ee0f2ae726f6402a91d99810e
mpijierro <mpijierro@gmail.com>
2019-03-18 01:04:23 +0100

    add home text

--- resources/views/resource/home.blade.php
+++ resources/views/resource/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="row" >
+        <div class="col-xs-12">
+            <h3 class="text-left description-category">
+                <b>Devoogle</b> tiene indexados actualmente <b>{{$total}}</b> recursos relaciones con el desarrollo de software.
+            </h3>
+        </div>
+    </div>
+
     @include('resource.list')
 
 @endsection
