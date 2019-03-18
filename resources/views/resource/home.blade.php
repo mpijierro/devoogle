c8eca100cde5e9816cece5f52da83729cdb489f4
mpijierro <mpijierro@gmail.com>
2019-03-18 01:05:08 +0100

    fix text

--- resources/views/resource/home.blade.php
+++ resources/views/resource/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row" >
         <div class="col-xs-12">
             <h3 class="text-left description-category">
-                <b>Devoogle</b> tiene indexados actualmente <b>{{$total}}</b> recursos relaciones con el desarrollo de software.
+                <b>Devoogle</b> tiene indexados actualmente <b>{{$total}}</b> recursos relacionados con el desarrollo de software.
             </h3>
         </div>
     </div>
