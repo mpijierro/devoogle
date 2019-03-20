d811640d36cca4337f809422eb67d9de088921b0
mpijierro <mpijierro@gmail.com>
2019-03-20 22:34:46 +0100

    add search test

---
+++ tests/Unit/Src/Search/Command/StoreSearchHandlerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit;
+
+use Devoogle\Src\Search\Command\StoreSearchCommand;
+use Devoogle\Src\Search\Command\StoreSearchHandler;
+use Devoogle\Src\Search\Model\Search;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class StoreSearchHandlerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testSaveSearchSuccesfully()
+    {
+
+        $command = new StoreSearchCommand('api rest cigüeña');
+        $handler = app(StoreSearchHandler::class);
+        $handler($command);
+
+        $search = Search::first();
+        $this->assertEquals('api rest cigüeña', $search->original());
+        $this->assertEquals('api-rest-ciguena', $search->original());
+        $this->assertEquals(1, $search->count());
+
+    }
+
+    public function testIncreasesTheCounter()
+    {
+
+        $command = new StoreSearchCommand('api rest');
+        $handler = app(StoreSearchHandler::class);
+        $handler($command);
+
+        $search = Search::where('slug', 'api-rest')->first();
+        $this->assertEquals(1, $search->count());
+
+        $command = new StoreSearchCommand('api rest');
+        $handler = app(StoreSearchHandler::class);
+        $handler($command);
+
+        $search = Search::where('slug', 'api-rest')->first();
+        $this->assertEquals(2, $search->count());
+
+    }
+
+}
