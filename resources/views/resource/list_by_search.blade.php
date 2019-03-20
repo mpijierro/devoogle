f667cc5206254b6a05b62b4362bad2d56c1db2db
mpijierro <mpijierro@gmail.com>
2019-03-20 22:22:25 +0100

    added spoiler in search

--- resources/views/resource/list_by_search.blade.php
+++ resources/views/resource/list_by_search.blade.php
@@ -5,6 +5,8 @@
     <h1><i class="fa fa-search " aria-hidden="true"></i> {!! $view->searchedText() !!} </h1>
     <span class="text-left description-category">
         Recursos de programación de {!! $view->searchedText() !!}
+        <br>
+        La búsqueda es un simple <b>LIKE '%...término...%'</b> ¯\_(ツ)_/¯<br>Muy pronto nos ayudará Elasticsearch a obtener mejores resultados.
     </span>
 
     @include('resource.list')
