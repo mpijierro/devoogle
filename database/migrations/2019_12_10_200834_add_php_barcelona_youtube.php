7df861aa47ece58be0950db901fb73b400caa6d1
mpijierro <mpijierro@gmail.com>
2019-12-10 20:10:49 +0100

    add youtube channel php barcelona

---
+++ database/migrations/2019_12_10_200834_add_php_barcelona_youtube.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhpBarcelonaYoutube extends Migration
+{
+    public function up()
+    {
+        DB::table('youtube_channel')->insert([
+            'source_id'       => 1,
+            'slug_id'         => 'UCBqwZ_Nvu9gSyB3VP4p1wcA',
+            'slug_name'       => 'php-barcelona',
+            'name'            => 'PHP Barcelona',
+            'is_user_channel' => false,
+
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
